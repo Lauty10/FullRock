358b0f109b20da21a01d0795e2a5710e27c0a699
Lautaro Maldonado <lautylty@hotmail.com>
2024-01-27 18:58:37 -0300

    cambios

--- controladores/carr.controladores.js
+++ controladores/carr.controladores.js
@@ -13,20 +13,20 @@ const carrProduct=async(req,res)=>{
 
 const deleteCarr=async(req,res)=>{
     try {
-        const  carrProd= await carrModel.findOne({_id:req.params.idCarrito});
+        const  carrProd= await carrModel.findOne({_id:req.idCarr});
         const productCarr= await modeloProducto.findOne({_id:req.params.idProductCarr})
 
-        const deleteNFav=carrProd.favoritos.filter((data)=>data._id.toString()!==productCarr._id.toString())
+        const deleteNFav=carrProd.productos.filter((data)=>data._id.toString()!==productCarr._id.toString())
 
-        const deleteAFav=carrProd.favoritos.filter((data)=>data._id.toString()===productCarr._id.toString())
+        const deleteAFav=carrProd.productos.filter((data)=>data._id.toString()===productCarr._id.toString())
 
         if (!deleteAFav.length) {
             return res.status(400).json({mensaje:"Id incorrecto"})
         }
 
-         carrProd.favoritos=deleteNFav
+         carrProd.productos=deleteNFav
          await carrProd.save()
-         res.status(200).json({mensaje:"favorito eliminado:"})
+         res.status(200).json({mensaje:"eliminado del carrito:"})
 
     } catch (error) {
         console.log(error);
